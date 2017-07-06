f812afe9bca189d9862e10110ae64d210fd979a3
Igor <igor@pr-cy.ru>
2017-07-06 15:58:48 +0300

    Change html special dd-dt

--- src/Complexes/View/Special.js
+++ src/Complexes/View/Special.js
@@ -8,41 +8,50 @@ class Available extends Component {
         <div className="complex-spec">
           <h2 className="complex-spec__title">Характеристики</h2>
           <Row>
-            <Col xs={2}>
-              <dt className="complex-spec__name">Количество квартир</dt>
-              <dt className="complex-spec__name">Статус</dt>
-              <dt className="complex-spec__name">Цены</dt>
-              <dt className="complex-spec__name">Безопасность</dt>
+            <Col xs={4}>
+              <dl className="complex-spec__wrap">
+                <dt className="complex-spec__name">Количество квартир</dt>
+                <dd className="complex-spec__data">1 503</dd>
+
+                <dt className="complex-spec__name">Статус</dt>
+                <dd className="complex-spec__data">Квартиры</dd>
+
+                <dt className="complex-spec__name">Цены</dt>
+                <dd className="complex-spec__data">от 5.3 до 143.5 млн</dd>
+
+                <dt className="complex-spec__name">Безопасность</dt>
+                <dd className="complex-spec__data">Охраняемая территория</dd>
+              </dl>
             </Col>
-            <Col xs={2}>
-              <dd className="complex-spec__data">1 503</dd>
-              <dd className="complex-spec__data">Квартиры</dd>
-              <dd className="complex-spec__data">от 5.3 до 143.5 млн</dd>
-              <dd className="complex-spec__data">Охраняемая территория</dd>
+            <Col xs={4}>
+              <dl className="complex-spec__wrap">
+                <dt className="complex-spec__name">Конструкция корпусов</dt>
+                <dd className="complex-spec__data">монолит</dd>
+
+                <dt className="complex-spec__name">Площадь</dt>
+                <dd className="complex-spec__data">от 50 до 154 м²</dd>
+
+                <dt className="complex-spec__name">Высота потолков</dt>
+                <dd className="complex-spec__data">3.45 − 5 м</dd>
+
+                <dt className="complex-spec__name">Обслуживание</dt>
+                <dd className="complex-spec__data">1 200 руб / м² / месяц</dd>
+              </dl>
             </Col>
-            <Col xs={2}>
-              <dt className="complex-spec__name">Конструкция корпусов</dt>
-              <dt className="complex-spec__name">Площадь</dt>
-              <dt className="complex-spec__name">Высота потолков</dt>             
-              <dt className="complex-spec__name">Обслуживание</dt>             
-            </Col>
-            <Col xs={2}>
-              <dd className="complex-spec__data">монолит</dd>
-              <dd className="complex-spec__data">от 50 до 154 м²</dd>
-              <dd className="complex-spec__data">3.45 − 5 м</dd>
-              <dd className="complex-spec__data">1 200 руб / м² / месяц</dd>
-            </Col>
-            <Col xs={2}>
-              <dt className="complex-spec__name">Начало строительства</dt>
-              <dt className="complex-spec__name">Конец строительства</dt>
-              <dt className="complex-spec__name">Наземная парковка</dt>             
-              <dt className="complex-spec__name">Подземная парковка</dt>             
-            </Col>
-            <Col xs={2}>
-              <dd className="complex-spec__data">I квартал 2012 года</dd>
-              <dd className="complex-spec__data">IV квартал 2018 года</dd>
-              <dd className="complex-spec__data">1 500 м/м</dd>
-              <dd className="complex-spec__data">Нет</dd>
+            <Col xs={4}>
+              <dl className="complex-spec__wrap">
+                <dt className="complex-spec__name">Начало строительства</dt>
+                <dd className="complex-spec__data">I квартал 2012 года</dd>
+
+                <dt className="complex-spec__name">Конец строительства</dt>
+                <dd className="complex-spec__data">IV квартал 2018 года</dd>
+
+                <dt className="complex-spec__name">Наземная парковка</dt>
+                <dd className="complex-spec__data">1 500 м/м</dd>
+
+                <dt className="complex-spec__name">Подземная парковка</dt>
+                <dd className="complex-spec__data">Нет</dd>
+              </dl>
             </Col>
           </Row>
         </div>
